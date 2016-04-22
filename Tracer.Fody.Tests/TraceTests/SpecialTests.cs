a87e070161adfb541c7254602e3d9195874f5676
csnemes <csaba.nemes@outlook.com>
2016-04-22 07:02:35 +0200

    added NoTrace weaving tests

--- Tracer.Fody.Tests/TraceTests/SpecialTests.cs
+++ Tracer.Fody.Tests/TraceTests/SpecialTests.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
 using NUnit.Framework;
+using Tracer.Fody.Filters;
 using Tracer.Fody.Tests.MockLoggers;
 
 namespace Tracer.Fody.Tests.TraceTests
@@ -44,6 +45,79 @@ namespace Tracer.Fody.Tests.TraceTests
         }
 
 
+        [Test]
+        public void NoTrace_Attribute_On_Class_Skips_Method()
+        {
+            string code = @"
+                using System;
+                using System.Diagnostics;
+                using System.Threading; 
 
+                namespace TracerAttributes
+                {
+                        [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+                        public class NoTrace : Attribute
+                        {
+                        }
+                }
+
+                namespace First
+                {
+                    using TracerAttributes;
+
+                    [NoTrace]
+                    public class MyClass
+                    {
+                        public static void Main()
+                        {
+                            Thread.Sleep(10);
+                        }
+                    }
+                }
+            ";
+
+            var def = new DefaultFilter.AssemblyLevelTraceDefinition(DefaultFilter.TraceTargetVisibility.All,
+                DefaultFilter.TraceTargetVisibility.Public);
+            var result = this.RunTest(code, new DefaultFilter(new [] { def }), "First.MyClass::Main");
+            result.Count.Should().Be(0);
+        }
+
+
+        [Test]
+        public void NoTrace_Attribute_On_Method_Skips_Method()
+        {
+            string code = @"
+                using System;
+                using System.Diagnostics;
+                using System.Threading; 
+
+                namespace TracerAttributes
+                {
+                        [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+                        public class NoTrace : Attribute
+                        {
+                        }
+                }
+
+                namespace First
+                {
+                    using TracerAttributes;
+
+                    public class MyClass
+                    {
+                        [NoTrace]
+                        public static void Main()
+                        {
+                            Thread.Sleep(10);
+                        }
+                    }
+                }
+            ";
+
+            var def = new DefaultFilter.AssemblyLevelTraceDefinition(DefaultFilter.TraceTargetVisibility.All,
+                DefaultFilter.TraceTargetVisibility.Public);
+            var result = this.RunTest(code, new DefaultFilter(new[] { def }), "First.MyClass::Main");
+            result.Count.Should().Be(0);
+        }
     }
 }
