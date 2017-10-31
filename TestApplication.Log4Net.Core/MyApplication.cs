fae22678305b0da6f43a8be1da3f2dd42cb2fd9d
csnemes <csaba.nemes@outlook.com>
2017-10-31 13:48:22 +0100

    added core test app

---
+++ TestApplication.Log4Net.Core/MyApplication.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace TestApplication.Log4Net.Core
+{
+    public class MyApplication
+    {
+        public void Run()
+        {
+            Thread.Sleep(500);
+        }
+    }
+}
