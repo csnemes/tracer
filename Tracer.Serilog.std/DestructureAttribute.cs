bd76656ae6f36f064348722b454be54aa64d8d4b
Duane McKinney <dmckinney@dabmi.com>
2017-11-04 07:00:19 -0700

    Added missing project.

---
+++ Tracer.Serilog.std/DestructureAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer.Serilog
+{
+    /// <summary>
+    /// Marker attribute for destructuring
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+    public class DestructureAttribute : Attribute
+    {
+    }
+
+    /// <summary>
+    /// Used for adding destructured types to the loggers list
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    public class DestructureTypeAttribute : Attribute
+    {
+        public DestructureTypeAttribute(Type typeToDestructure)
+        {
+            TypeToDestructure = typeToDestructure;
+        }
+
+        public Type TypeToDestructure { get; }
+    }
+}
