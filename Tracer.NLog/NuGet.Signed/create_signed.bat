75234edb8dabe33dc5ef9ac6270c4a6ed328b313
csnemes <csaba.nemes@outlook.com>
2018-05-14 15:00:08 +0200

    created nuget packages

--- Tracer.NLog/NuGet.Signed/create_signed.bat
+++ Tracer.NLog/NuGet.Signed/create_signed.bat
@@ -1,4 +1,4 @@
-del .\lib\net40\*.dll /F
+del .\lib\netstandard2.0\*.dll /F
 del .\Tracer.NLog.il /F
-ildasm ..\NuGet\lib\net40\Tracer.NLog.dll /out:.\Tracer.NLog.il
-ilasm .\Tracer.NLog.il /DLL /key=.\Tracer.NLog.snk /output=.\lib\net40\Tracer.NLog.dll
+ildasm ..\NuGet\lib\netstandard2.0\Tracer.NLog.dll /out:.\Tracer.NLog.il
+ilasm .\Tracer.NLog.il /DLL /key=.\Tracer.NLog.snk /output=.\lib\netstandard2.0\Tracer.NLog.dll
